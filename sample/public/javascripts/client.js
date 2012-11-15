d142f0a66e9021b21c482cbdffa3c0a745e3de02
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-15 18:26:57 +0900

    クライアント側の実装は完了

--- sample/public/javascripts/client.js
+++ sample/public/javascripts/client.js
@@ -4,8 +4,30 @@ jQuery(function($) {
    var socket = io.connect();
    console.log("クライアントで接続"); 
 
-   //sendボタンがクリックされたとき
+   //入力されたらする処理。
+   $("#input").keyup(function(){
+      var text = $(this).val();
+      //ここに入力する。
+      $("#comment").text(text);
+   });  
 
+   
+   //sendボタンがクリックされたとき
+   $("#send").click(function(){
+      console.log("クリックされたー");
+      var text = $('#input').val();
+      
+      if(text !== ''){
+         //サーバーにテキストを送信
+         socket.emit("message",{text:text});
+         $('#list').prepend($('<div/>').text(text));
+         $("#input").val("");
+         $("#comment").text("");
+      }
+   
+   });
+   
+       
 
 
 
